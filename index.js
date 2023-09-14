9df0d053c00b13318c8515111ba7f3ea55017711
Diogo Borges <NB29456@novabase.pt>
2023-09-14 15:36:03 +0100

    merged stuff

--- index.js
+++ index.js
@@ -5,9 +5,15 @@ const app = express();
 const mongoose = require("mongoose");
 const port = process.env.PORT || 3000;
 const db_connection_string = process.env.DB_CONNECTION_STRING;
+
+// Routes
 const user = require('./route/user.route.js');
 const trip = require('./route/trip.route.js');
 const auth = require('./route/auth.route.js');
+
+// Documentation
+const swaggerUi = require("swagger-ui-express"); 
+const swaggerDocument = require("./swagger.json");
 
 mongoose.set("strictQuery", false);
 mongoose
@@ -20,7 +26,11 @@ mongoose
 
 app.use(express.json());
 
+// Documentação
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, { explorer: true })); 
+
 app.get("/", (req, res) => res.status(200).send({ name: "Travel.r API", version: "0.1" }));
+app.get('/tripadvisor/locations/:locationname', require("./controller/trip.controller.js").getSuggestions);
 
 // Users API
 app.use('/user', user);
