5190052676a5bd2a5f6e03400a8c2895d1c68ca5
Nuno Dias <nuno.dias@celfocus.com>
2023-09-15 11:27:39 +0100

    fixed problems with register

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -26,25 +26,26 @@ require('dotenv').config()
   
   // Register route
     exports.register = async (req, res) => {
-      const { username, name, password,identificationDoc,vaccination} = req.body;
+      //const { username, name, password,identificationDoc,vaccination} = req.body;
+      const register_data = req.body;
 
       // Check if the username is already taken
-      existingUser = await User.findOne({ username: username});
+      existingUser = await User.findOne({ username: register_data.username});
       if (existingUser != null) {
         return res.status(400).json({ error: 'Username already taken' });
       }
     
       // Hash the password before saving
-      hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
+      hashedPassword = await bcrypt.hash(register_data.password, SALT_ROUNDS);
   
       // Create a new user with isAdmin set to false
       const newUser = new User({
-        username : username,
-        name : name,
+        username : register_data.username,
+        name : register_data.name,
         password: hashedPassword,
         isAdmin : false,
-        identificationDoc: identificationDoc,
-        vaccination, vaccination
+        identificationDoc: register_data.identificationDoc,
+        vaccination: register_data.vaccination
       }
       );
     
