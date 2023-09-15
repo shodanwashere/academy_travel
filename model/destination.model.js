fd3506f565e89a601ef2c2105becbf66ca9fad99
Jorge Cardoso <NB29575@novabase.pt>
2023-09-15 15:11:00 +0100

    Addendum to destination model

--- model/destination.model.js
+++ model/destination.model.js
@@ -25,37 +25,21 @@ const schema = new mongoose.Schema({
     type: mongoose.Schema.Types.Number,
     required: [true, "Enter a valid value for 'emergencyNumber'"]
   },
-  attendees: [{
-    user: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "user",
-      required: [true, "Enter a valid value for 'user'"]
-    },
-    ticket: {
-      type: mongoose.Schema.Types.String,
-      ref: "user",
-      required: [true, "Enter a valid value for 'ticket'"]
-    },
-    reservation: {
-      type: mongoose.Schema.Types.String,
-      ref: "user",
-      required: [true, "Enter a valid value for 'reservation'"]
-    },
-  }],
-  itinerary: [{
-    name: {
-      type: mongoose.Schema.Types.String,
-      required: [true, "Enter a valid value for 'name'"]
-    },
-    description: {
-      type: mongoose.Schema.Types.String,
-      required: [true, "Enter a valid value for 'description'"]
-    },
-    date: {
-      type: mongoose.Schema.Types.Date,
-      required: [true, "Enter a valid value for 'date'"]
-    }
-  }]
+  ticket: {
+    type: mongoose.Schema.Types.String,
+    required: [true, "Enter a valid value for 'ticket'"]
+  },
+  reservation: {
+    type: mongoose.Schema.Types.String,
+    required: [true, "Enter a valid value for 'reservation'"]
+  },
+  itenerary: [{
+    poi:{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "poi",
+    required: [true, "Enter a valid value for 'poi'"] 
+  }
+ }]
 });
 
-exports.Trip = mongoose.model("trip", schema);
+exports.Trip = mongoose.model("destination", schema);
